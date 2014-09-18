ab28d96cbf7feff021b42805787eec7920ef5f93
Leonid Suprun <mr.slay@gmail.com>
2014-09-18 23:55:04 +0400

    shorten macro

--- fjk.h
+++ fjk.h
@@ -1,9 +1,6 @@
 #define CHUNKS 3
 #define set_io(fname, stdpipe, var, fmode, errno) if (strlen(fname)) {\
   if ((var = fopen(fname, fmode)) == NULL) {\
-    perror("Unable to open " #var " file");\
-    exit(errno);\
+    perror("Unable to open " #var " file"); exit(errno);\
   }\
-} else {\
-  var = stdpipe;\
-}
+} else { var = stdpipe; }
