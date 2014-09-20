295d713a1e5adaf0a20d8aaf8a5f4511050eb717
Leonid Suprun <mr.slay@gmail.com>
2014-09-20 15:26:02 +0400

    Less stinky code

--- python.c
+++ python.c
@@ -6,10 +6,10 @@
 
 #include "include/fjk.h"
 
-static PyObject * py_fjk_encode(PyObject *self, PyObject *args);
-static PyObject * py_fjk_decode(PyObject *self, PyObject *args);
+static PyObject* py_fjk_encode(PyObject *self, PyObject *args);
+static PyObject* py_fjk_decode(PyObject *self, PyObject *args);
 
-static PyMethodDef SpamMethods[] = {
+static PyMethodDef FjkMethods[] = {
     {"encode",  py_fjk_encode, METH_VARARGS, "Encode string."},
     {"decode",  py_fjk_decode, METH_VARARGS, "Decode string."},
     {NULL, NULL, 0, NULL}        /* Sentinel */
@@ -18,19 +18,22 @@ static PyMethodDef SpamMethods[] = {
 PyMODINIT_FUNC initfjk(void) {
   PyObject *m;
 
-  m = Py_InitModule("fjk", SpamMethods);
+  m = Py_InitModule("fjk", FjkMethods);
   if (m == NULL)
       return;
 }
 
-static PyObject * py_fjk_encode(PyObject *self, PyObject *args) {
+PyObject* _py_fjk_wrap(PyObject *args, fjk_algo method) {
   const char *s;
-  PyArg_ParseTuple(args, "s", &s);
-  return Py_BuildValue("s#", fjk_encrypt(s, strlen(s)), strlen(s));
+  if (!PyArg_ParseTuple(args, "s", &s))
+    return NULL;
+  return Py_BuildValue("s#", method(s, strlen(s)), strlen(s));
 }
 
-static PyObject * py_fjk_decode(PyObject *self, PyObject *args) {
-  const char *s;
-  PyArg_ParseTuple(args, "s", &s);
-  return Py_BuildValue("s#", fjk_decrypt(s, strlen(s)), strlen(s));
+static PyObject* py_fjk_encode(PyObject *self, PyObject *args) {
+  return _py_fjk_wrap(args, fjk_encrypt);
+}
+
+static PyObject* py_fjk_decode(PyObject *self, PyObject *args) {
+  return _py_fjk_wrap(args, fjk_decrypt);
 }
