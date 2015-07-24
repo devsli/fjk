e9eaeb483d299d85046d31b06a2cfefdfc822495
Leonid Suprun <mr.slay@gmail.com>
2015-07-24 11:16:14 +0300

    Help message updated

--- fjk.c
+++ fjk.c
@@ -61,8 +61,9 @@ void run(FILE *from, FILE *to, fjk_algo method)
 
 void print_usage(char *selfname)
 {
-	printf("Usage: %s [-d] [-i infile] [-o outfile]\n", selfname);
-	printf("	-d: decrypt\n");
+	printf("Usage: %s [-d] [-i infile] [-o outfile] [-h]\n", selfname);
+	printf("	-d:      decrypt\n");
 	printf("	-i FILE: input file, or STDIN\n");
 	printf("	-o FILE: output file, or STDOUT\n");
+	printf("	-h:      display this help and exit\n");
 }
